60464c821ac0723be8b2d7546871c3468c8c59ca
Tanor <tanor98@mail.ru>
2023-03-17 15:01:58 +0500

    added work with Mysql,Sqlite and a page for creating an article

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_data.db'
+#  Способ работы с mysql таблицей, где name = имя подключения и password = пароль от подключения sql
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://name:pasword@localhost:3306/flask_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    intro = db.Column(db.String(250), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
@@ -14,9 +36,23 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):
-    return 'User page ' + name + " - " + str(id)
+@app.route('/create-article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'При добавлении статьи произошла ошибка!'
+    else:
+        return render_template('create-article.html')
 
 
 if __name__ == "__main__":
